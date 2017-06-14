ea8dd87150e29a918aba852134134490eb0237b3
James Ferguson <james.ferguson7@gmail.com>
2017-06-14 11:18:30 +1000

    Generate Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InverseOfInRails403::Application.config.secret_key_base = '4c959ffa8e4b0f355a50b71c297fd104d992b536dbb317f0ab71a56934d4ffbf06a2db37babf48ccea500f2ad5039bf1ff3a5d486c1cc86b89b6b565373f12a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InverseOfInRails403::Application.config.session_store :cookie_store, key: '_inverse_of_in_rails_4_0_3_session'
