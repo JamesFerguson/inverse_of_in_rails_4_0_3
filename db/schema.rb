044c18bac222f39c3a38075ecc35d1331e45c17b
James Ferguson <james.ferguson7@gmail.com>
2017-06-14 18:04:39 +1000

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614013256) do
+ActiveRecord::Schema.define(version: 20170614075407) do
+
+  create_table "article_invs", force: true do |t|
+    t.string   "name"
+    t.integer  "author_inv_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "article_invs", ["author_inv_id"], name: "index_article_invs_on_author_inv_id"
+
+  create_table "articles", force: true do |t|
+    t.string   "name"
+    t.integer  "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "articles", ["author_id"], name: "index_articles_on_author_id"
+
+  create_table "author_invs", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "authors", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "criminal_invs", force: true do |t|
     t.string   "name"
@@ -24,6 +54,24 @@ ActiveRecord::Schema.define(version: 20170614013256) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "email_invs", force: true do |t|
+    t.string   "name"
+    t.integer  "user_inv_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "email_invs", ["user_inv_id"], name: "index_email_invs_on_user_inv_id"
+
+  create_table "emails", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "emails", ["user_id"], name: "index_emails_on_user_id"
 
   create_table "prison_invs", force: true do |t|
     t.string   "name"
@@ -58,5 +106,17 @@ ActiveRecord::Schema.define(version: 20170614013256) do
 
   add_index "sentences", ["criminal_id"], name: "index_sentences_on_criminal_id"
   add_index "sentences", ["prison_id"], name: "index_sentences_on_prison_id"
+
+  create_table "user_invs", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
