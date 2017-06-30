5e8e0e543478afc8eff77e0cbbfc6f9a2f160b55
James Ferguson <james.ferguson7@gmail.com>
2017-06-30 11:00:52 +1000

    Prove that inverse_of on belongs_to for has_many does nothing

--- app/models/article_inv.rb
+++ app/models/article_inv.rb
@@ -1,3 +1,3 @@
 class ArticleInv < ActiveRecord::Base
-  belongs_to :author_inv
+  belongs_to :author_inv, inverse_of: :article_invs # inverse_of here does nothing
 end
--- test/models/sentence_inv_test.rb
+++ test/models/sentence_inv_test.rb
@@ -27,17 +27,31 @@ class SentenceInvTest < ActiveSupport::TestCase
   # describe "inverse_of on simple has_many/belongs_to relations"
   #
   test "without inverse_of on has_many articles.author triggers a db hit" do
-    author = Author.create(name: 'Harry')
+    author = Author.create(name: 'Rita Skeeter')
     article = author.articles.create(name: 'A History of Quidditch')
 
     assert_not_equal article.author.object_id, author.object_id
   end
 
   test "with inverse_of on has_many articles.author no db hit and object_ids are equal/one instance" do
-    iauthor = AuthorInv.create(name: 'Harry')
+    iauthor = AuthorInv.create(name: 'Rita Skeeter')
     iarticle = iauthor.article_invs.create(name: 'A History of Quidditch')
 
     assert_equal iarticle.author_inv.object_id, iauthor.object_id
+  end
+
+  test "without inverse_of on belongs_to author.articles triggers a db hit" do
+    article = Article.create(name: 'A History of Quidditch')
+    author = article.author = Author.create(name: 'Rita Skeeter')
+
+    assert_not_equal author.articles.first.object_id, article.object_id
+  end
+
+  test "with inverse_of on belongs_to author.articles still triggers db hit - has no benefit" do
+    iarticle = ArticleInv.create(name: 'A History of Quidditch')
+    iauthor = iarticle.author_inv = AuthorInv.create(name: 'Rita Skeeter')
+
+    assert_not_equal iauthor.article_invs.first.object_id, iarticle.object_id
   end
 
   # describe "inverse of on belongs_to in join models ensures the join instance is saved
