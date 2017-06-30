5a8ed069b8a87c08d6537c176c4f9f5cf1c261a8
James Ferguson <james.ferguson7@gmail.com>
2017-06-30 15:35:28 +1000

    Add accepts_nested_attributes_for w validation for testing

--- app/models/article_inv.rb
+++ app/models/article_inv.rb
@@ -1,3 +1,5 @@
 class ArticleInv < ActiveRecord::Base
   belongs_to :author_inv, inverse_of: :article_invs # inverse_of here does nothing
+
+  validates :author_inv, presence: true
 end
--- app/models/author_inv.rb
+++ app/models/author_inv.rb
@@ -1,3 +1,5 @@
 class AuthorInv < ActiveRecord::Base
   has_many :article_invs, inverse_of: :author_inv
+
+  accepts_nested_attributes_for :article_invs
 end
