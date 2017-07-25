84a1b542ef4973024e092db8b77e0d3d6acef0be
James Ferguson <james.ferguson7@gmail.com>
2017-07-25 12:14:32 +1000

    Comment that inv_of does nothing on has_many :through

--- app/models/has_many through/criminal_inv.rb
+++ app/models/has_many through/criminal_inv.rb
@@ -1,4 +1,4 @@
 class CriminalInv < ActiveRecord::Base
   has_many :sentence_invs, inverse_of: :criminal_inv
-  has_many :prison_invs, through: :sentence_invs #, inverse_of: :criminal_invs
+  has_many :prison_invs, through: :sentence_invs #, inverse_of: :criminal_invs # pretty sure this does nothing
 end
--- app/models/has_many through/prison_inv.rb
+++ app/models/has_many through/prison_inv.rb
@@ -1,4 +1,4 @@
 class PrisonInv < ActiveRecord::Base
   has_many :sentence_invs, inverse_of: :prison_inv
-  has_many :criminal_invs, through: :sentence_invs #, inverse_of: :prisons
+  has_many :criminal_invs, through: :sentence_invs #, inverse_of: :prisons # pretty sure this does nothing
 end
